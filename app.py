959c3095cc33aa51994001af3d7b5758bd28f2d4
Figlia <amajiddddd@gmail.com>
2025-11-08 22:39:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -93,6 +93,4 @@ def recommend_api():
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
